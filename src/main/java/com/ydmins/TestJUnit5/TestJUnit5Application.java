ffc00c3a7ec4318a1e63188e0e4209643742d664
ydmins <dr.ydmins@gmail.com>
2023-09-10 19:50:52 +0900

    first commit

---
+++ src/main/java/com/ydmins/TestJUnit5/TestJUnit5Application.java
@@ -0,0 +1,13 @@
+package com.ydmins.TestJUnit5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestJUnit5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestJUnit5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ydmins/TestJUnit5/TestJUnit5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ydmins.TestJUnit5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestJUnit5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
